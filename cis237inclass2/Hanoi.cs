d4bffe457ba9de21358c83bf015b3f5dbb017783
Paul Bath <paulbath7@gmail.com>
2016-02-03 19:10:52 -0500

    Added Towers of Hanoi

---
+++ cis237inclass2/Hanoi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass2
+{
+    class Hanoi
+    {
+        public void moveDisk(int n, char source, char auxilary, char destination)
+        {
+            //Base Case to exit recursive loop
+            if (n == 1)
+            {
+                //Since we only have to move one disk. (n==1), we dont want to make another recursive call.  
+                // we just want ot ouput the move that we are performing
+                Console.WriteLine("Move disk from tower {0} to tower {1}", source, destination);
+            }
+            else
+            {
+                // Note: the desctriptions of the source, aux, and desstination are reffering to 
+                // the fist time this moveDisk method is called.
+                // As recursion works, it may not mae sense to assocaiate the work source with a peg letter
+
+
+                // Attempt to move (n-1) disks from souce location (A) to the aux (B) to the destination (C)
+                // Using the C peg as the temp storage
+                moveDisk(n - 1, source, destination, auxilary);
+
+                // Move the Source disk (A) to the Destination (C) using Aux (B) as temp storage
+                moveDisk(1, source, auxilary, destination);
+
+                // Move the (n-1) from Aux (B) to the Destination (C) using the beginning (A) as temp storage
+                moveDisk(n - 1, auxilary, source, destination);
+
+            }
+
+        }
+    }
+}
--- cis237inclass2/Program.cs
+++ cis237inclass2/Program.cs
@@ -10,7 +10,40 @@ namespace cis237inclass2
     {
         static void Main(string[] args)
         {
-            Factoria factorial = new Factoria();
+            Console.WriteLine("Welcome! \n Please enter the program you would like to use:" +
+                                "\n 1. for Factorials"+
+                                "\n 2. for Towers of Hanoi");
+            String userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "1" :
+                    Factoria factorial = new Factoria();
+                    break;
+
+                case "2" :
+                    Console.WriteLine("Enter the number of disks \n 'Hint: enter 5 or lower'");
+                    userInput = Console.ReadLine();
+                    try
+                    {
+                        //Do Hanoi Here
+                        // Create a new instance of the hanoi class
+                        Hanoi hanoi = new Hanoi();
+
+                        //call the moveDisk metod to start solving the problems
+                        hanoi.moveDisk(Int32.Parse(userInput), 'A', 'B', 'C');
+                    }
+                    catch
+                    {
+                        Console.WriteLine("You must enter an int");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Input ya tart! \n :(");
+                    break;
+            }
+            
         }
     }
 }
